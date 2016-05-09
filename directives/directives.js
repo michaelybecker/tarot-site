78157e028471d9f791fd8f2110e126e2814b2f05
Michael Hazani <michael@michaelhazani.com>
2016-05-09 10:17:29 -0700

    add webstorm stuff

--- directives/directives.js
+++ directives/directives.js
@@ -10,14 +10,14 @@ template: '<h1>Card Index</h1>'+
     '<div class="row cardspace">' +
     '<div class="col-xs-12 cardtitle">' +
             '<h2>{{ deck.activeCard.name }}</h3>'+
-            '</div></div>' + 
+            '</div></div>' +
     '<div class="row carddetails">' +
         '<div class="col-xs-12 card-place">'+
             '<img class="img-responsive pull-left card-image" src="{{ deck.activeCard.image }}">'+
-            '<h3>History</h3>'+
-            '<p ng-bind-html="deck.activeCard.history"></p>'+
-            '<h3>Interpretation</h3>' +
-            '<p ng-bind-html="deck.activeCard.interpretation"></p>' +
+            // '<h3>History</h3>'+
+            '<p ng-bind-html="deck.activeCard.PKT"></p>' +
+            // '<h3>Interpretation</h3>' +
+            // '<p ng-bind-html="deck.activeCard.interpretation"></p>' +
         '</div>'
     // '</div>'
 };
